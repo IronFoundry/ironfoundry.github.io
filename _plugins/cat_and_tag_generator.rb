cb9a23dce93d4feba1f88b2a51cecf661823847e
adrien lo <adrien.lo@tier3.com>
2013-08-21 14:50:29 -0700

    - added tags and categories

--- _plugins/cat_and_tag_generator.rb
+++ _plugins/cat_and_tag_generator.rb
@@ -16,7 +16,14 @@ module Jekyll
 
     def build_subpages(site, type, posts) 
       posts[1] = posts[1].sort_by { |p| -p.date.to_f }     
+      atomize(site, type, posts)
       paginate(site, type, posts)
+    end
+
+    def atomize(site, type, posts)
+      path = "/#{type}/#{posts[0]}"
+      atom = AtomPage.new(site, site.source, path, type, posts[0], posts[1])
+      site.pages << atom
     end
 
     def paginate(site, type, posts)
@@ -46,6 +53,21 @@ module Jekyll
       self.read_yaml(File.join(base, '_layouts'), "group_index.html")
       self.data["grouptype"] = type
       self.data[type] = val
+    end
+  end
+  
+  class AtomPage < Page
+    def initialize(site, base, dir, type, val, posts)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'atom.xml'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), "group_atom.xml")
+      self.data[type] = val
+      self.data["grouptype"] = type
+      self.data["posts"] = posts[0..9]
     end
   end
 end
